b9f01711e8c6dd305e8b189dea05bdb0a80f4c4a
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-09 13:40:47 +0530

    Create pop_2.py

---
+++ List Methods/pop/pop_2.py
@@ -0,0 +1,4 @@
+#pop()-return the value which elelment has been deleted
+fruits=['strawbery','orange','banana','apple','mango','apple']
+fruits.pop()
+print(fruits)// prints the whole list after removing the last element
