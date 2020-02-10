d88cf60433363d751e9232864fc4dba5f5108995
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-10 11:47:38 +0530

    Create find.py

---
+++ String_Buit-in_Functions/find.py
@@ -0,0 +1,5 @@
+#String Built-in Functions 
+#find(str,start,end)-used to find the first occourence of the string 
+a="hello welcome to python proggramming language"
+res=a.find('l',0,len(a))
+print(res)
