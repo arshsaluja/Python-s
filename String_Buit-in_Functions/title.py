09f20e43b151ab8bdb1a0d2396c2ef7850c49d8a
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-10 11:56:00 +0530

    Create title.py

---
+++ String_Buit-in_Functions/title.py
@@ -0,0 +1,6 @@
+#String Buit-in Functions
+#title()- unlike istitle() function it will not check the string
+#it will change all the first letters of the string to capital
+a="hello welcome to python proggramming language"
+res=a.title()
+print(res)
