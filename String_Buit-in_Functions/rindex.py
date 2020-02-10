bd43e84d37515a1a06becb6422eef305ddbda92c
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-10 11:55:05 +0530

    Create rindex.py

---
+++ String_Buit-in_Functions/rindex.py
@@ -0,0 +1,7 @@
+#String Buit-in Functions
+#rindex(str,start,end)-prints the last occourence
+#whereas index(str,start,end)prints the first occourence
+#same as rfind function
+a="hello welcome to python proggramming language"
+res=a.rindex('l',0,len(a))
+print(res)
