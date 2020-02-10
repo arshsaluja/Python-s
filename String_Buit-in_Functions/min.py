2e32c9a80bf7a29e3536e885f3a888c332f8cfd2
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-10 11:52:37 +0530

    Create min.py

---
+++ String_Buit-in_Functions/min.py
@@ -0,0 +1,6 @@
+#String Built-in Functions 
+#min(value)-returns the minimum of the letter in the string 
+a="hello welcome to python proggramming language"
+res=min(a)
+print(res)
+#as space has the minimum so it prits space
