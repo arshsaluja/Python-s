71bba4303b720af30f442eac0f52f7734c5f89a9
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-02-10 11:49:47 +0530

    Create istitle.py

---
+++ String_Buit-in_Functions/istitle.py
@@ -0,0 +1,6 @@
+#String Built-in Functions 
+#istitle()-tells if the first letter of each word in the string is capital or not
+#gives a boolean value
+a="Hello Welcome To Python Proggramming Language"
+res=a.istitle()
+print(res)
