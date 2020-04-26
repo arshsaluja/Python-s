0cd08a776a151d75f733774478ee11c2d3a955d9
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-27 04:01:38 +0530

    Create first.py

---
+++ name/first.py
@@ -0,0 +1,12 @@
+//is this file directly being run by python or is being imported
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def main():
+    print("First Module's name")
+    print(__name__)
+if __name__=='__main__':
+    main()
