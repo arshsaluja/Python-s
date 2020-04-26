6e8d72e45d764195ad54dcfddf1a83a7693155e7
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-04-27 04:03:29 +0530

    Create second.py

---
+++ name/second.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 23:58:34 2020
+
+@author: DELL
+"""
+
+import first
+def main():
+    print("Second Module's name=  ")
+    print(__name__);
+if __name__=='main':
+   main()
+   
+  
