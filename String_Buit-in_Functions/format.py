1ee55b268d74e04bc17f64413858e2e343f08e78
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-06-06 06:10:09 +0530

    Create format.py

---
+++ String_Buit-in_Functions/format.py
@@ -0,0 +1,5 @@
+country = "France"
+capital = "Paris"
+sentence = "The capital of {} is {}.".format(country, capital)#here curly brackets as a replacement string 
+print(sentence)
+#o/p of the code is - The capital of France is Paris
