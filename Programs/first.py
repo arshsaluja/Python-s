f8ceeb37b1904bf5bccb8444c2681bba2dc1b1a2
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-06-07 05:41:24 +0530

    Program that takes a string ad counts the no of lower case vowels in it

--- Programs/first.py
+++ Programs/first.py
@@ -1,3 +1,10 @@
+#Program that takes a string ad counts the no of lower case vowels in it
+"""
+Write a function count_vowels(word) that takes the string word as input 
+and returns the number of occurrences of lowercase vowels 
+(i.e. the lowercase letters "aeiou") in word. 
+Hint: Python has a built-in string method that can count the number of occurrences of a letter in a string.
+"""
 def isVowel(ch):
     return ch in ['a','e','i','o','u']
 def count_vowels(word):
