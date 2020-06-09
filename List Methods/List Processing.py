0c83cce9ea2aad2d3a4e98ad1a32a5d086615835
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-06-10 03:37:56 +0530

    Update List Processing.py

--- List Methods/List Processing.py
+++ List Methods/List Processing.py
@@ -5,3 +5,7 @@ Simple task list.
 def new(tasklist, task):
     """Add new task"""
     tasklist.append(task)
+def remove_by_num(tasklist, tasknum):
+    """Remove by number"""
+    if tasknum > 0 and tasknum <= len(tasklist):
+        tasklist.pop(tasknum - 1)
