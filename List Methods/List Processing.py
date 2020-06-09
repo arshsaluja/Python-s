f5822706073bed3e1452f4af88d8c39c74b97d73
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-06-10 03:38:14 +0530

    Update List Processing.py

--- List Methods/List Processing.py
+++ List Methods/List Processing.py
@@ -9,3 +9,7 @@ def remove_by_num(tasklist, tasknum):
     """Remove by number"""
     if tasknum > 0 and tasknum <= len(tasklist):
         tasklist.pop(tasknum - 1)
+def remove_by_name(tasklist, taskname):
+    """Remove by name"""
+    if taskname in tasklist:
+        tasklist.remove(taskname)
