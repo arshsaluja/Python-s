3aaf8332b0ed88bac97d809cfc4fc4b9cec94ef6
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-06-15 02:42:14 +0530

    Update read.py

--- Files/read.py
+++ Files/read.py
@@ -2,4 +2,7 @@ openfile = open("gonewiththewind.txt", "rt")
 
 filedata = openfile.read()
 print(filedata)
-//read-returns the entire contents of the file as a string
+#read-returns the entire contents of the file as a string
+
+#never forget to close the file
+openfile.close()
